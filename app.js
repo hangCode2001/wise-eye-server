e63f19d6f2e61b7868d0f5a2ba9a1829e390649f
魏航宇 <weihangyu@bytedance.com>
2022-06-11 18:13:33 +0800

    跨域

--- app.js
+++ app.js
@@ -40,6 +40,19 @@ app.use(
     extension: "pug",
   })
 );
+app.use(async (ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  ctx.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+  );
+  ctx.set("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  if (ctx.method == "OPTIONS") {
+    ctx.body = 200;
+  } else {
+    await next();
+  }
+});
 
 router.prefix("/api"); //设置路由前缀
 
